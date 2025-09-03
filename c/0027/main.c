7ffeca0302d70ea121704ab7d61c1572eb24170b
Mistivia <i@mistivia.com>
2025-09-03 16:07:06 +0800

    solve 27

--- c/0027/main.c
+++ c/0027/main.c
@@ -1,3 +1,11 @@
 int removeElement(int* nums, int numsSize, int val) {
-    
+    int p = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == val) {
+            continue;
+        }
+        nums[p] = nums[i];
+        p++;
+    }
+    return p;
 }
